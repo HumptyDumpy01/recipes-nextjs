b6de9827a54f15c4c6f7d620df6a9ed9c3967838
HumptyDumpy01 <tuznikolas@gmail.com>
2025-03-04 14:47:50 +0200

    Implemented the ability to filter recipes in / (home) page using inputs.

--- components/Recipes/Recipes.tsx
+++ components/Recipes/Recipes.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Button, Container, Grid, GridItem, Heading, Image, Spinner, Text } from '@chakra-ui/react';
+import { Box, Button, Container, Grid, GridItem, Heading, Image, Input, Select, Spinner, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import axios from 'axios';
@@ -16,6 +16,8 @@ export default function Recipes() {
   const [title, setTitle] = useState('All Recipes');
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [filterType, setFilterType] = useState('');
+  const [filterValue, setFilterValue] = useState('');
   const itemsPerPage = 8;
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -34,7 +36,7 @@ export default function Recipes() {
         }
 
         const response = await axios.get(url);
-        setRecipes(response.data.meals);
+        setRecipes(response.data.meals || []);
       } catch (error) {
         console.error('Error fetching recipes:', error);
       } finally {
@@ -57,8 +59,19 @@ export default function Recipes() {
     setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
   };
 
+  const handleFilterSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    router.push(`/?filterType=${filterType}&filterValue=${filterValue}`);
+  };
+
+  const handleClearFilters = () => {
+    setFilterType('');
+    setFilterValue('');
+    router.push('/');
+  };
+
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const selectedRecipes = recipes.slice(startIndex, startIndex + itemsPerPage);
+  const selectedRecipes = Array.isArray(recipes) ? recipes.slice(startIndex, startIndex + itemsPerPage) : [];
 
   if (loading) {
     return (
@@ -72,30 +85,55 @@ export default function Recipes() {
     <>
       <Container maxW="container.xl" py={4}>
         <Heading as="h1" size="xl" mb={6}>{title}</Heading>
-        <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6} justifyContent="center"
-              alignItems="center">
-          {selectedRecipes.map(recipe => (
-            <GridItem
-              width="250px"
-              key={recipe.idMeal}
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              cursor="pointer"
-              onClick={() => handleRecipeClick(recipe.idMeal)}
-              _hover={{ boxShadow: 'lg' }}
-            >
-              <Image src={recipe.strMealThumb} alt={recipe.strMeal} boxSize="250px" objectFit="cover" />
-              <Box p={4}>
-                <Text textAlign="center" fontSize="lg" fontWeight="semibold">{recipe.strMeal}</Text>
-              </Box>
-            </GridItem>
-          ))}
-        </Grid>
-        <Box display="flex" justifyContent="center" gap={`15px`} mt={4}>
-          <Button onClick={handlePreviousPage} isDisabled={currentPage === 1}>Previous</Button>
-          <Button onClick={handleNextPage} isDisabled={startIndex + itemsPerPage >= recipes.length}>Next</Button>
+        <Box as="form" onSubmit={handleFilterSubmit} mb={6} display="flex" gap={4}>
+          <Select placeholder="Select filter type" value={filterType} onChange={(e) => setFilterType(e.target.value)}>
+            <option value="name">Name</option>
+            <option value="ingredient">Ingredient</option>
+            <option value="category">Category</option>
+            <option value="country">Country</option>
+          </Select>
+          <Input
+            placeholder="Enter filter value"
+            value={filterValue}
+            onChange={(e) => setFilterValue(e.target.value)}
+          />
         </Box>
+        <Box mb={10} display="flex" justifyContent="center" gap={4}>
+          <Button type="submit">Filter</Button>
+          <Button onClick={handleClearFilters}>Clear Filters</Button>
+        </Box>
+        {selectedRecipes.length === 0 ? (
+          <Box textAlign="center" mt={4}>
+            <Text>No items to be seen</Text>
+          </Box>
+        ) : (
+          <>
+            <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6} justifyContent="center"
+                  alignItems="center">
+              {selectedRecipes.map(recipe => (
+                <GridItem
+                  width="250px"
+                  key={recipe.idMeal}
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  overflow="hidden"
+                  cursor="pointer"
+                  onClick={() => handleRecipeClick(recipe.idMeal)}
+                  _hover={{ boxShadow: 'lg' }}
+                >
+                  <Image src={recipe.strMealThumb} alt={recipe.strMeal} boxSize="250px" objectFit="cover" />
+                  <Box p={4}>
+                    <Text textAlign="center" fontSize="lg" fontWeight="semibold">{recipe.strMeal}</Text>
+                  </Box>
+                </GridItem>
+              ))}
+            </Grid>
+            <Box display="flex" justifyContent="center" gap={`15px`} mt={4}>
+              <Button onClick={handlePreviousPage} isDisabled={currentPage === 1}>Previous</Button>
+              <Button onClick={handleNextPage} isDisabled={startIndex + itemsPerPage >= recipes.length}>Next</Button>
+            </Box>
+          </>
+        )}
       </Container>
     </>
   );
